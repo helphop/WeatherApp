d33e2c98e8a903040d3fc3f9b1b49a44a243e8b3
Mitchell Gould <mitchellgould7@gmail.com>
2024-09-25 11:28:31 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mitchell Gould on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
